f79cbeb0118da6d0f1e60fe3b277c803ff16621d
Utitofon-Udoekong <mosesrahul37@gmail.com>
2023-11-28 22:28:58 +0100

    Add javascript to handle switchikng between task. Switch between task header and body. Toggle check state for both task and body images.

--- app.js
+++ app.js
@@ -1,25 +1,144 @@
 const HIDE_CLASS = 'hide';
 const OPEN_CLASS = 'open'
 const TASK_DONE = 'done';
+
 // As a merchant, when I click on the notification bell, 
 // I see a dropdown panel that shows me an empty list of notifications since I have none yet. 
 // When I click on the notification bell again, this dropdown panel is closed.
 
 const notificationBell = document.querySelector(".notification-bell");
 const notificationDropDown = document.querySelector(".notifications");
-const showNotifications = () => notificationDropDown.classList.toggle(OPEN_CLASS);
-notificationBell.addEventListener("click",showNotifications)
+const notificationMenuActions = notificationDropDown.querySelector(".title .actions").children;
+const notificationMenuItems = Array.from(notificationMenuActions)
+
+// Handle traversing through the notification menu dropdown with the arrow keys
+const handleNotificationMenuItemTraverse = (event, index) => {
+    const isFirstItem = index === 0;
+    const isLastItem = notificationMenuItems.length - 1 === index;
+
+    const nextItem = notificationMenuItems[index + 1]
+    const previousItem = notificationMenuItems[index - 1]
+    // If arrow right or down 
+    if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+        if (isLastItem) {
+            // If on the last item, focus the first item
+            notificationMenuItems[0].focus()
+            return;
+        }
+        // focus the next item
+        nextItem.focus()
+    }
+
+    // If arrow left or up focus the previous element
+    if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+        if (isFirstItem) {
+            // If on the first item, focus the last item
+            notificationMenuItems[notificationMenuItems.length - 1].focus()
+            return;
+        }
+        // focus the next item
+        previousItem.focus()
+    }
+}
+
+const handleNotificationMenuEscapePress = (event) => {
+    if (event.key === "Escape") {
+        closeNotificationMenu()
+    }
+}
+
+const openNotificationMenu = () => {
+    notificationDropDown.classList.add(OPEN_CLASS)
+    notificationBell.ariaExpanded = "true"
+    notificationMenuItems[0].focus()
+    // Close the notifications menu with the escape key
+    notificationDropDown.addEventListener("keyup", (event) => handleNotificationMenuEscapePress(event))
+    notificationMenuItems.forEach((item, index) => item.addEventListener("keyup", (event) => handleNotificationMenuItemTraverse(event, index)))
+}
+const closeNotificationMenu = () => {
+    notificationDropDown.classList.remove(OPEN_CLASS)
+    notificationBell.ariaExpanded = "false"
+    notificationBell.focus()
+}
+const toggleNotificationMenu = () => {
+    const isnotificationDropDownExpanded = notificationDropDown.classList.contains(OPEN_CLASS)
+
+    if (isnotificationDropDownExpanded) {
+        closeNotificationMenu()
+    } else {
+        openNotificationMenu()
+    }
+}
+notificationBell.addEventListener("click", toggleNotificationMenu)
+
 
 // As a merchant, when I click on the name of my store Davii Collections, 
 // or on my profile image placeholder DC on the far right of the topbar menu, 
 // I see a menu with a list of menu items as specified by the Figma design. 
 // Clicking on this button again closes the menu
-
-const profileDropDown = document.querySelector(".profile-settings");
 const profileBar = document.querySelector('.profile-bar')
-const showProfileSettings = () => profileDropDown.classList.toggle(OPEN_CLASS)
-profileBar.addEventListener("click",showProfileSettings)
+const profileDropDown = document.querySelector(".profile-settings");
+const profileMenuItems = profileDropDown.querySelectorAll('[role="menuitem"]')
 
+// Handle traversing throught the profile settings menu dropdown with the arrow keys
+const handleMenuItemTraverse = (event, index) => {
+    const isFirstItem = index === 0;
+    const isLastItem = profileMenuItems.length - 1 === index;
+
+    const nextItem = profileMenuItems.item(index + 1)
+    const previousItem = profileMenuItems.item(index - 1)
+    // If arrow right or down 
+    if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+        if (isLastItem) {
+            // If on the last item, focus the first item
+            profileMenuItems.item(0).focus()
+            return;
+        }
+        // focus the next item
+        nextItem.focus()
+    }
+
+    // If arrow left or up focus the previous element
+    if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+        if (isFirstItem) {
+            // If on the first item, focus the last item
+            profileMenuItems.item(profileMenuItems.length - 1).focus()
+            return;
+        }
+        // focus the next item
+        previousItem.focus()
+    }
+}
+// Handle closing a the profile menu when the escape key is pressed
+const handleProfileMenuEscapePress = (event) => {
+    if (event.key === "Escape") {
+        closeProfileMenu()
+    }
+}
+
+const openProfileMenu = () => {
+    profileDropDown.classList.add(OPEN_CLASS)
+    profileBar.ariaExpanded = "true"
+    profileMenuItems.item(0).focus()
+    // Close the profile key with the escape key
+    profileDropDown.addEventListener("keyup", (event) => handleProfileMenuEscapePress(event))
+    profileMenuItems.forEach((item, index) => item.addEventListener("keyup", (event) => handleMenuItemTraverse(event, index)))
+}
+const closeProfileMenu = () => {
+    profileDropDown.classList.remove(OPEN_CLASS)
+    profileBar.ariaExpanded = "false"
+    profileBar.focus()
+}
+const toggleProfileMenu = () => {
+    const isProfileDropdownExpanded = profileDropDown.classList.contains(OPEN_CLASS)
+
+    if (isProfileDropdownExpanded) {
+        closeProfileMenu()
+    } else {
+        openProfileMenu()
+    }
+}
+profileBar.addEventListener("click", toggleProfileMenu)
 
 
 
@@ -28,36 +147,41 @@ profileBar.addEventListener("click",showProfileSettings)
 const banner = document.querySelector('.banner')
 const closeBannerBtn = banner.querySelector('.action .close-btn')
 const closeBanner = () => banner.classList.add(HIDE_CLASS)
-closeBannerBtn.addEventListener("click",closeBanner)
+closeBannerBtn.addEventListener("click", closeBanner)
+
 
 const taskDropdownButton = document.querySelector('.setup-guide .header .dropdown');
-const taskHeader = document.querySelector('.setup-guide .header')
+const taskDropdown = document.querySelector('.setup-guide .header')
 const taskContents = document.querySelector('.setup-guide .content')
+
+const taskList = Array.from(document.querySelectorAll(".task"));
+const taskHeaders = Array.from(document.querySelectorAll(".task .head .task-title"))
+const progressTitle = document.querySelector('.progress p')
+const progressBar = document.querySelector('.progress-bar .value')
+
+//Handle setting completion state of tasks
+let progressTextContent = document.getElementById('current-progress')
+let currentProgress = 0
 
 // As a merchant, when I click the "Arrow Up" icon on the top right of the Setup guide card, 
 // it closes the card. When I click again, it reopens the card.
 const showTasks = () => {
     taskContents.classList.toggle(OPEN_CLASS);
-    taskHeader.classList.toggle(OPEN_CLASS)
+    taskDropdown.classList.toggle(OPEN_CLASS)
 }
-taskDropdownButton.addEventListener('click',showTasks)
+taskDropdownButton.addEventListener('click', showTasks)
 
-//Handle setting completion state of tasks
-const tasks = document.querySelectorAll(".task")
-const taskList = Array.from(tasks);
-const progressTitle = document.querySelector('.progress p')
-const progressBar = document.querySelector('.progress-bar .value')
-let progressTextContent = document.getElementById('current-progress')
-let currentProgress = 0
+
+
 // As a merchant, when I click on any of the 5 onboarding steps, it expands the panel, 
 // showing the content of the onboarding step, and closes the previously opened one. 
 // When I click on an opened onboarding step, nothing happens.
 const toggleTaskVisibility = (task) => {
-    if(task.classList.contains(OPEN_CLASS)){
+    if (task.classList.contains(OPEN_CLASS)) {
         return;
-    }else{
-        let openTask = taskList.find((task)=>task.classList.contains(OPEN_CLASS))
-        if(openTask){
+    } else {
+        let openTask = taskList.find((task) => task.classList.contains(OPEN_CLASS))
+        if (openTask) {
             openTask.classList.remove(OPEN_CLASS);
         }
         task.classList.add(OPEN_CLASS)
@@ -81,45 +205,60 @@ const decrementProgress = () => {
 // left side of an onboarding step title, it marks that step as completed, 
 // and expands the next incomplete step. If I click again, it marks the step as incomplete. 
 
-const toggleTaskCompletion = (taskImage, task, index) => {
-    const notCompletedIcon = taskImage.querySelector('.unchecked')
-    const completedIcon = taskImage.querySelector('.checked')
-    const loadingIcon = taskImage.querySelector('.loading')
+const toggleTaskCompletion = (taskBodyImage, taskHeaderImage, task, index) => {
+    /// Code is not optimal
+    // TODO: Refactorcode to change only one instance of taskImage
+    const bodyNotCompletedIcon = taskBodyImage.querySelector('.unchecked')
+    const bodyCompletedIcon = taskBodyImage.querySelector('.checked')
+    const bodyLoadingIcon = taskBodyImage.querySelector('.loading')
+    const headNotCompletedIcon = taskHeaderImage.querySelector('.unchecked')
+    const headCompletedIcon = taskHeaderImage.querySelector('.checked')
+    const headLoadingIcon = taskHeaderImage.querySelector('.loading')
     const isTaskDone = task.classList.contains(TASK_DONE)
-    if(isTaskDone){
+    if (isTaskDone) {
         // Mark task as not done
-        completedIcon.classList.remove(OPEN_CLASS)
-        loadingIcon.classList.add(OPEN_CLASS)
+        bodyCompletedIcon.classList.remove(OPEN_CLASS)
+        headCompletedIcon.classList.remove(OPEN_CLASS)
+        bodyLoadingIcon.classList.add(OPEN_CLASS)
+        headLoadingIcon.classList.add(OPEN_CLASS)
         setTimeout(() => {
-            loadingIcon.classList.remove(OPEN_CLASS)
-            notCompletedIcon.classList.add(OPEN_CLASS)
+            bodyLoadingIcon.classList.remove(OPEN_CLASS)
+            headLoadingIcon.classList.remove(OPEN_CLASS)
+            bodyNotCompletedIcon.classList.add(OPEN_CLASS)
+            headNotCompletedIcon.classList.add(OPEN_CLASS)
             task.classList.remove(TASK_DONE)
-            taskImage.ariaLabel = taskImage.ariaLabel.replace("as not done", "as done")
+            taskBodyImage.ariaLabel = taskBodyImage.ariaLabel.replace("as not done", "as done")
+            taskHeaderImage.ariaLabel = taskHeaderImage.ariaLabel.replace("as not done", "as done")
             decrementProgress()
         }, 2000)
-    }else{
+    } else {
         // Mark task as done
-        notCompletedIcon.classList.remove(OPEN_CLASS)
-        loadingIcon.classList.add(OPEN_CLASS)
+        bodyNotCompletedIcon.classList.remove(OPEN_CLASS)
+        headNotCompletedIcon.classList.remove(OPEN_CLASS)
+        bodyLoadingIcon.classList.add(OPEN_CLASS)
+        headLoadingIcon.classList.add(OPEN_CLASS)
         setTimeout(() => {
-            loadingIcon.classList.remove(OPEN_CLASS)
-            completedIcon.classList.add(OPEN_CLASS)
+            bodyLoadingIcon.classList.remove(OPEN_CLASS)
+            headLoadingIcon.classList.remove(OPEN_CLASS)
+            bodyCompletedIcon.classList.add(OPEN_CLASS)
+            headCompletedIcon.classList.add(OPEN_CLASS)
             task.classList.add(TASK_DONE)
-            taskImage.ariaLabel = taskImage.ariaLabel.replace("as done", "as not done")
+            taskBodyImage.ariaLabel = taskBodyImage.ariaLabel.replace("as done", "as not done")
+            taskHeaderImage.ariaLabel = taskHeaderImage.ariaLabel.replace("as done", "as not done")
             incrementProgress()
 
             //Focus the next incomplete step
-            const nextIncompleteTask = taskList[index+1];
-            const previousIncompleteTask = taskList[index-1];
+            const nextIncompleteTask = taskList[index + 1];
+            const previousIncompleteTask = taskList[index - 1];
 
-            if(index === taskList.length - 1){
+            if (index === taskList.length - 1) {
                 const closestIncompleteTask = taskList.find((t) => t.classList.contains(TASK_DONE) === false);
                 toggleTaskVisibility(closestIncompleteTask);
-            }else if(nextIncompleteTask.classList.contains(TASK_DONE) === false){
+            } else if (nextIncompleteTask.classList.contains(TASK_DONE) === false) {
                 toggleTaskVisibility(nextIncompleteTask);
-            }else if(previousIncompleteTask.classList.contains(TASK_DONE) === false){
+            } else if (previousIncompleteTask.classList.contains(TASK_DONE) === false) {
                 toggleTaskVisibility(previousIncompleteTask);
-            }else{
+            } else {
                 toggleTaskVisibility(taskList[taskList.length - 1]);
             }
         }, 2000)
@@ -128,13 +267,42 @@ const toggleTaskCompletion = (taskImage, task, index) => {
 }
 
 
-
-for(var index = 0; index < taskList.length; index++){
-    let task = taskList[index];
-    task.addEventListener("click",() => {
+for (var index = 0; index < taskHeaders.length; index++) {
+    let header = taskHeaders[index];
+    let task = header.parentElement.parentElement
+    header.addEventListener("click", () => {
         toggleTaskVisibility(task)
     })
-    let taskImage = task.querySelector(".body #task-image")
-    taskImage.addEventListener("click", () => toggleTaskCompletion(taskImage, task, taskList.indexOf(task)))
+    let taskBodyImage = task.querySelector('.task-image')
+    let taskHeaderImage = task.querySelector('.task-header-image')
+    taskBodyImage.addEventListener("click", () => toggleTaskCompletion(taskBodyImage,taskHeaderImage, task, taskList.indexOf(task)))
+    taskHeaderImage.addEventListener("click", () => toggleTaskCompletion(taskBodyImage,taskHeaderImage, task, taskList.indexOf(task)))
 }
 
+/// Click event listener that listens for clicks outside of the notification and profile dropdown
+// menus. If regsitered, it closes the corresponding menu
+document.addEventListener(
+    "click",
+    function (event) {
+        const isProfileDropdownExpanded = profileDropDown.classList.contains(OPEN_CLASS)
+        const isnotificationDropDownExpanded = notificationDropDown.classList.contains(OPEN_CLASS)
+        // Check if the notifications dropdown is open
+        if (isnotificationDropDownExpanded) {
+            if (
+                // Check if the target is not the notification dropdown or the notification-bar
+                !event.target.closest("#notifications") && !event.target.closest(".notification-bar")
+                ) {
+                closeNotificationMenu()
+            } 
+            return;
+        }
+        // Check if the profile dropdown is open
+        if (isProfileDropdownExpanded) {
+            // Check if the target is not the profile dropdown or the notification-bar
+            if (!event.target.closest('#profile-settings') && !event.target.closest(".notification-bar")) {
+                closeProfileMenu()
+            }
+            return;
+        }
+    },
+)
